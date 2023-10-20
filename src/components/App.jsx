5b2f20d4f4185b21ac70cf35a6ba01e8685ca48d
astasiempre <ero1494651@gmail.com>
2023-10-20 21:34:43 +0300

    update files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,28 @@
-
-import { useState, useEffect } from 'react';
+// import { useEffect } from 'react';
 import ContactForm from './ContactForm/ContactForm';
 import Filter from './Filter/Filter';
 import ContactList from './ContactList/Contactlist';
-import {Title} from './App.styled'
+import { Title } from './App.styled';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+// import { type } from '@testing-library/user-event/dist/type';
+import { addContacts, deleteContacts, setFilter } from 'redux/contactsReducer';
+export const App = () => {
+  // const [contacts, setContacts] = useState([
+  //   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  //   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  //   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  //   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  // ]);
+  // const [filter, setFilter] = useState('');
 
+  const contacts = useSelector(state => state.contacts.contacts);
+  const filter = useSelector(state => state.contacts.filter);
+  const dispatch = useDispatch();
 
-
-const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
-  const [filter, setFilter] = useState('');
-
-  const handleAddContact = (newContact) => {
+  const handleAddContact = newContact => {
     const phoneBookHasContact = contacts.some(
-      (contact) => contact.name.toLowerCase() === newContact.name.toLowerCase()
+      contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
     );
 
     if (phoneBookHasContact) {
@@ -26,42 +30,48 @@ const App = () => {
       return;
     }
 
-    setContacts((prevContacts) => [...prevContacts, newContact]);
+    //   setContacts(prevContacts => [...prevContacts, newContact]);
+    // dispatch({ type: 'contacts/setContacts', payload: newContact });
+    dispatch(addContacts(newContact));
   };
 
-  const handleDeleteContact = (contactName) => {
-    setContacts((prevContacts) =>
-      prevContacts.filter((contact) => contact.name !== contactName)
+    const handleDeleteContact = contactName => {
+      // setContacts(prevContacts =>
+      //   prevContacts.filter(contact => contact.name !== contactName)
+      // );
+      // const deleteContact = contacts.filter(contact => contact.name !== contactName);
+      dispatch(deleteContacts(contactName));
+    };
+
+    const handleFilterChange = filter => {
+      // setFilter(filter);
+      dispatch(setFilter(filter));
+    };
+
+    // useEffect(() => {
+    //   const storedContacts = localStorage.getItem('contacts');
+    //   if (storedContacts) {
+    //     // setContacts(JSON.parse(storedContacts));
+    //     dispatch({ type: 'contacts/setContacts', payload: JSON.parse(storedContacts) })
+    //   }
+    // }, [dispatch]);
+
+    // useEffect(() => {
+    //   localStorage.setItem('contacts', JSON.stringify(contacts));
+    // }, [contacts]);
+
+    return (
+      <div>
+        <ContactForm onAddContact={handleAddContact} contacts={contacts} />
+        <Title>Contacts</Title>
+        <Filter filter={filter} onFilterChange={handleFilterChange} />
+        <ContactList
+          contacts={contacts}
+          filter={filter}
+          onDeleteContact={handleDeleteContact}
+        />
+      </div>
     );
   };
 
-  const handleFilterChange = (filter) => {
-    setFilter(filter);
-  };
 
-  useEffect(() => {
-    const storedContacts = localStorage.getItem('contacts');
-    if (storedContacts) {
-      setContacts(JSON.parse(storedContacts));
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
-  return (
-    <div>
-      <ContactForm onAddContact={handleAddContact} contacts={contacts} />
-      <Title>Contacts</Title>
-      <Filter filter={filter} onFilterChange={handleFilterChange} />
-      <ContactList
-        contacts={contacts}
-        filter={filter}
-        onDeleteContact={handleDeleteContact}
-      />
-    </div>
-  );
-};
-
-export default App;
